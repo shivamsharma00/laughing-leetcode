4b362f56c68e81d966e4f24024a6c843a3956953
Shivam Sharma <54909615+shivamsharma00@users.noreply.github.com>
2024-03-27 12:32:05 -0700

    added heap solution

---
+++ problems/767 Reorganize String.py
@@ -0,0 +1,64 @@
+'''
+767. Reorganize String
+Solved
+Medium
+Topics
+Companies
+Hint
+Given a string s, rearrange the characters of s so that any two adjacent characters are not the same.
+
+Return any possible rearrangement of s or return "" if not possible.
+
+ 
+
+Example 1:
+
+Input: s = "aab"
+Output: "aba"
+Example 2:
+
+Input: s = "aaab"
+Output: ""
+ 
+
+Constraints:
+
+1 <= s.length <= 500
+s consists of lowercase English letters.
+
+'''
+import heapq
+
+class Solution:
+    def reorganizeString(self, s: str) -> str:
+        
+        # count frequency of each character
+        freq_map = {}
+        for char in s:
+            freq_map[char] = freq_map.get(char, 0) + 1
+        
+        # build max heap based on frequency
+        max_heap = [(-freq, char) for char, freq in freq_map.items()]
+        heapq.heapify(max_heap)
+
+        # initialize result string
+        res = []
+
+        # alternate placing the most common letters
+        while len(max_heap) >= 2:
+            freq1, char1 = heapq.heappop(max_heap)
+            freq2, char2 = heapq.heappop(max_heap)
+
+            res.extend([char1, char2])
+
+            if freq1+1 < 0:
+                heapq.heappush(max_heap, (freq1+1, char1))
+            if freq2+1 < 0:
+                heapq.heappush(max_heap, (freq2+1, char2))
+
+        if max_heap:
+            freq, char = heapq.heappop(max_heap)
+            if -freq > 1:
+                return ""
+            res.append(char)
+        return "".join(res)
